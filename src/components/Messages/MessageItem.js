492e2a119fddd31de6518b1ae66bd813c5866c1a
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-02-12 11:17:43 -0600

    add icons buttons

---
+++ src/components/Messages/MessageItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+//page will display each message on a card that can be clicked  
+
+
+class MessageItem extends Component {
+
+  cardClick = () => {
+      console.log('Card Click');
+
+  }
+        
+    render() {
+        return (
+            <Card className="messageCard">
+            <CardActions onClick={this.cardClick}>
+            <CardContent>
+              <Typography variant="h6" className="headline">
+                {this.props.message.headline}
+              </Typography>
+              <Typography className="dateUser">
+                {this.props.message.date}, User:{this.props.message.user_id}
+              </Typography>
+            </CardContent>
+            
+              {/* <Button onClick={this.cardClick} size="small">Learn More</Button> */}
+            </CardActions>
+          </Card>
+        );
+    }
+}
+
+
+  
+export default MessageItem;
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import axios from 'axios';
-
+import MessageItem from './MessageItem.js';
+import './Messages.css';
 class Messages extends Component {
     state = {
         messageList: [],
@@ -10,27 +11,30 @@ class Messages extends Component {
     componentDidMount(){
         this.getMessageList();
     }
-
+        // GET  request for the message to display on the dashboard
     getMessageList = () => {
         axios.get('/api/messages')
         .then( response => {
-            console.log(response);
             this.setState({
                 messageList:  response.data 
             })
         })
+    };// end Message Get
+
+    addMessage = () => {
+        console.log('add Message');
     }
 
     render() {
         return (
             <div>
-                {JSON.stringify(this.state.messageList)}
-                <ul>{this.state.messageList.map(message =>
-                    (<li key={message.mess_id}>{message.headline}<br />
-                        Posted by: {message.user_id} at:
-                        {message.date}</li>)
+                {/* {JSON.stringify(this.state.messageList)} */}
+                {this.state.messageList.map(message =>
+                     <MessageItem key={message.mess_id} message={message} />
                 )}
-                </ul>
+                <div className="MessageBtn" >
+                    <button className="addMessageBtn" onClick={this.addMessage}>< i class="material-icons md-48"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></i></button>
+                </div>
             </div>
         );
     }
